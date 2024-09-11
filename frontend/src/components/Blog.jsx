0c0f7f6353f60af8bc8769f656ba0f2f3440a0b1
David Perez <deividpa022@gmail.com>
2024-09-11 13:43:51 -0500

    feat: Part 5.11 completed. Now its possible to delete blogs

--- frontend/src/components/Blog.jsx
+++ frontend/src/components/Blog.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const Blog = ({ blog, onLikeBlog, onDeleteBlog }) => {
-  const [visible, setVisible] = useState(false);
+    const [visible, setVisible] = useState(false);
 
   const blogStyle = {
     padding: 15,
@@ -12,36 +12,38 @@ const Blog = ({ blog, onLikeBlog, onDeleteBlog }) => {
   };
 
   const toggleVisibility = () => {
-    setVisible(!visible);
+        setVisible(!visible);
   };
 
   return (
     <div style={blogStyle}>
-      <div>
-        {blog.title} {blog.author}{' '}
-        <button onClick={toggleVisibility}>
-          {visible ? 'hide' : 'view'}
-        </button>
-        <button onClick={() => onDeleteBlog(blog.id)}>Delete</button>
-      </div>
-      {visible && (
         <div>
-          <p>{blog.url}</p>
-          <p>
-            likes: {blog.likes}
-            <button onClick={() => onLikeBlog(blog.id)} style={{marginLeft: 5}}>like</button>
-          </p>
-          <p>{blog.user.name}</p>
+            {blog.title} {blog.author}{' '}
+            <button onClick={toggleVisibility}>
+            {visible ? 'hide' : 'view'}
+            </button>
         </div>
-      )}
+        {visible && (
+            <div>
+                <p>{blog.url}</p>
+                <p>
+                    likes: {blog.likes}
+                    <button onClick={() => onLikeBlog(blog.id)} style={{marginLeft: 5}}>like</button>
+                </p>
+                <p>{blog.user.name}</p>
+                <div>
+                    <button onClick={() => onDeleteBlog(blog.id)}>Delete</button>
+                </div>
+            </div>
+        )}
     </div>
   );
 };
 
 Blog.propTypes = {
-  blog: PropTypes.object.isRequired,
-  onLikeBlog: PropTypes.func.isRequired,
-  onDeleteBlog: PropTypes.func.isRequired,
+    blog: PropTypes.object.isRequired,
+    onLikeBlog: PropTypes.func.isRequired,
+    onDeleteBlog: PropTypes.func.isRequired,
 };
 
 export default Blog;
